bfba56513b616ba12cc5b330d62f56378474db3a
wanghj <hj.wang@opencsg.com>
2026-07-07 10:50:22 +0800

    style(template): restore import grouping

--- internal/template/local_store.go
+++ internal/template/local_store.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	toml "github.com/pelletier/go-toml/v2"
 	"os"
 	"path/filepath"
 	"slices"
 	"strings"
 	"time"
+
+	toml "github.com/pelletier/go-toml/v2"
 )
 
 const (
